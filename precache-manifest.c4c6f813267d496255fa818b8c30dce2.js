3f040715ea35e837342540c3aa6fd59bc235790b
Ranveer Dhaliwal <ranveerdhaliwal@Ranveers-MBP.attlocal.net>
2021-02-21 15:54:23 -0800

    Updates

---
+++ precache-manifest.c4c6f813267d496255fa818b8c30dce2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f37e88744a6d5921ae6a76d56ae09b",
+    "url": "/DrumKit/index.html"
+  },
+  {
+    "revision": "a8784a485f3e23b37523",
+    "url": "/DrumKit/static/css/main.07060c22.chunk.css"
+  },
+  {
+    "revision": "1cd5bebed36e2055682a",
+    "url": "/DrumKit/static/js/2.df9e615e.chunk.js"
+  },
+  {
+    "revision": "8d4abe0b2dc5bd3c4887c8f93c1f20e2",
+    "url": "/DrumKit/static/js/2.df9e615e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8784a485f3e23b37523",
+    "url": "/DrumKit/static/js/main.8e8e39ca.chunk.js"
+  },
+  {
+    "revision": "e71fec93f053f060bf44",
+    "url": "/DrumKit/static/js/runtime-main.90ccb5cf.js"
+  }
+]);
